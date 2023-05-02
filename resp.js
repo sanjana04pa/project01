60616b33cf912e8e24299af97f39aa98c16476e3
sanjana04pa <91687238+sanjana04pa@users.noreply.github.com>
2023-05-02 21:29:18 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,13 @@
+burger =document.querySelector('.burger')
+navbar =document.querySelector('.navbar')
+navList =document.querySelector('.nav-list')
+rightNav =document.querySelector('.rightNav')
+
+
+
+burger.addEventListner('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    
+    navbar.classList.toggle('v-class-resp');
+})
